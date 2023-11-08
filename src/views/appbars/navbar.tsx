6be8edf53df10582e72e638a2b143289cf38ac71
sazidhayatkhan <sazidhayatkhan@gmail.com>
2023-11-08 08:46:30 +0600

    cart added

--- src/views/appbars/navbar.tsx
+++ src/views/appbars/navbar.tsx
@@ -1,14 +1,19 @@
 "use client"
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import { usePathname } from 'next/navigation';
+import { useSelector } from 'react-redux';
+import {BsFillTrash3Fill} from 'react-icons/bs'
 import {AiOutlineShoppingCart,AiOutlineClose} from 'react-icons/ai'
 import {GiHamburgerMenu} from 'react-icons/gi'
 import SideDrawer from '@/components/drawers/SideDrawer';
 import CartDrawer from '@/components/drawers/CartDrawer';
 
+
 const Navbar = () => {
   const pathname = usePathname()
+  const cartProducts = useSelector((state:any) => state.cart)
   const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
   const [isCartOpen, setIsCartOpen] = React.useState(false);
 
@@ -58,9 +63,33 @@ const Navbar = () => {
           </div>
         </SideDrawer>
         <CartDrawer isOpen={isCartOpen} onClose={closeCart}>
-          <div className='bg-purple-400 flex justify-between items-center p-4'>
-            <p className='text-dela'>Shopping Cart</p>
-            <button onClick={()=>setIsCartOpen(false)}><AiOutlineClose/></button>
+          <div>
+            <div className='bg-purple-400 flex justify-between items-center p-4'>
+              <p className='text-dela'>Shopping Cart</p>
+              <button onClick={()=>setIsCartOpen(false)}><AiOutlineClose/></button>
+            </div>
+            {
+              cartProducts?.map((item:any,i:any)=>(
+                <div key={i} className='grid grid-cols-4 text-black border-b-[1px] p-3'>
+                    <div className=''>
+                      <Image 
+                        src={item?.thumbnail || "/images/ss.png"}
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        className="w-[40px] h-[30px] md:w-[70px] md:h-[60px] object-cover"
+                      />
+                    </div>
+                    <div className='col-span-3 flex justify-between items-center'>
+                      <div>
+                        <p className='text-sm md:text-lg font-medium'>{item?.title}</p>
+                        <p className='text-xs md:text-sm font-semibold'>Price: BDT {item?.price}</p>
+                      </div>
+                      <button><BsFillTrash3Fill/></button>
+                    </div>
+                </div>
+              ))
+            }
           </div>
         </CartDrawer>
     </div>
